bbb33016b5c19970129557c8ce6ba33064866721
Mampenda <amalie.mampenda@gmail.com>
2024-10-11 11:02:54 +0200

    added dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ dependencies {
     implementation(platform("org.hibernate.orm:hibernate-platform:6.6.0.Final"))
     implementation("org.hibernate.orm:hibernate-core")
     implementation("jakarta.transaction:jakarta.transaction-api")
+    implementation("org.postgresql:postgresql:42.7.4")
     implementation("com.h2database:h2:2.2.220")
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.3")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
